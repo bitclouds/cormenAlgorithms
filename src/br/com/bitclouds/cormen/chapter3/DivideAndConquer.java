9d94c9e8e8761b0e6a3cf1dd5822a31fc2443200
Anderson Bittencourt <anderson_btt@hotmail.com>
2017-09-11 09:12:58 -0300

    Verbose added

--- src/br/com/bitclouds/cormen/chapter3/DivideAndConquer.java
+++ src/br/com/bitclouds/cormen/chapter3/DivideAndConquer.java
@@ -1,8 +1,10 @@
 package br.com.bitclouds.cormen.chapter3;
 
+import br.com.bitclouds.cormen.utils.sortingUtil;
+
 public class DivideAndConquer {
 	public static void main(String[] args) {
-		int[] A = {-1,1,-1,-1,2,-1};
+		int[] A = {-1,1,-1,3,2,-1};
 		int low = 0;
 		int high = A.length-1;
 		MaxSubarray maxSubarray = findMaxSubarray(A, low, high);
@@ -10,12 +12,18 @@ public class DivideAndConquer {
 	}
 
 	public static MaxSubarray findMaxSubarray(int[] A, int low, int high){
+		System.out.println("Input: A: "+sortingUtil.toString(A)+"; low: "+low+"; high: "+high);
 		if(high == low){
+				System.out.println("When high==low; high: "+high+"; low: "+low+"; A[low]: "+A[low]);
 				return new MaxSubarray(low, high, A[low]);
 		}else{ 
+			System.out.println("Calculates ; low: "+low+"; high: "+high);
 			int mid = (low+high)/2;
+			System.out.println("findMaxSubarray(A, low, mid); A: "+sortingUtil.toString(A)+"; low: "+low+"; mid: "+mid);
 			MaxSubarray leftSubarray = findMaxSubarray(A, low, mid);
+			System.out.println("findMaxSubarray(A, mid+1, high); A: "+sortingUtil.toString(A)+"; mid+1: "+mid+1+"; high: "+high);
 			MaxSubarray rightSubarray= findMaxSubarray(A, mid+1, high);
+			System.out.println("findMaxCrossingSubarray(A, low, mid, high); A: "+sortingUtil.toString(A)+"; low: "+low+"; mid: "+mid+"; high: "+high);
 			MaxSubarray crossSubarray= findMaxCrossingSubarray(A, low, mid, high);
 			if ((leftSubarray.sum>=rightSubarray.sum)&&(leftSubarray.sum>=crossSubarray.sum)){
 				return(leftSubarray);
@@ -30,15 +38,19 @@ public class DivideAndConquer {
 	}
 	
 	public static MaxSubarray findMaxCrossingSubarray(int[] A, int low, int mid, int high){
+		System.out.println("maxLeft, maxRight = 0");
 		int maxLeft = 0, maxRight = 0;
+		System.out.println("leftsum: -999999");
 		int leftSum = -999999;
-		System.out.println("leftsum: "+leftSum);
-		int sum = 0;
 		System.out.println("sum: 0");
+		int sum = 0;
+		System.out.println("for i = mid; i >= low; i--");
 		for (int i = mid; i >= low; i--) {
+			System.out.println("index: "+i);
 			System.out.println(sum+"+="+A[i]);
 			sum = sum+A[i];
 			System.out.println("sum="+sum);
+			System.out.println("leftSum: "+leftSum);
 			if(sum > leftSum){
 				System.out.println("leftSum="+leftSum);
 				leftSum = sum;
@@ -46,10 +58,10 @@ public class DivideAndConquer {
 				maxLeft = i;
 			}
 		}
+		System.out.println("rightSum: -999999");
 		int rightSum = -999999;
-		System.out.println("rightSum: "+rightSum);
-		sum = 0;
 		System.out.println("sum: 0");
+		sum = 0;
 		for (int i = mid+1; i <= high; i++) {
 			System.out.println(sum+"+="+A[i]);
 			sum = sum+A[i];
